7db0749a766c03f21d2694416da641a419e3c543
Mingli Yuan <mingli.yuan@gmail.com>
2016-01-19 18:20:42 +0800

    add webhook

--- src/webhook/main.py
+++ src/webhook/main.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask
 
 application = Flask(__name__)
 route = application.route
 application.debug = True
 
-@route('/')
+@route('/qps/caiyun-backend-wrapper-new')
 def index():
-	return "Hello World"
+    cmd = "cd caiyun-backend-wrapper-new"
+    cmd = "%s; source develop; bin/pref-test" % cmd
+    os.system("ssh caiyun@inner.badge.caiyunapp.com '%s'" % cmd)
+    return "OK"
 
 def main():
-	application.debug = True
-	application.run()
+    application.debug = True
+    application.run()
 
 if __name__ == "__main__":
-	main()
+    main()
