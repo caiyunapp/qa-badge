da6a98f9346d593edc7e49b6757f0998426da026
Mingli Yuan <mingli.yuan@gmail.com>
2016-01-20 01:28:20 +0800

    typo

--- src/webhook/main.py
+++ src/webhook/main.py
@@ -10,8 +10,8 @@ application.debug = True
 
 @route('/qps/caiyun-backend-wrapper-new')
 def index():
-    cwd = os.getcwd()
-    cmd = "%s/bin/perf-test" % cwd
+    cwd = os.path.basename(os.getcwd())
+    cmd = "%s/bin/perf-test" % os.path.join(cwd, 'caiyun-backend-wrapper-new')
     os.system("ssh caiyun@inner.bench.caiyunapp.com '%s'" % cmd)
     return "OK"
 
