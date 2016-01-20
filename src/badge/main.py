f57ccf4b3277223039ff06987d345ac81603dc0b
Mingli Yuan <mingli.yuan@gmail.com>
2016-01-20 09:02:06 +0800

    typo

--- src/badge/main.py
+++ src/badge/main.py
@@ -3,10 +3,15 @@
 import os
 
 from flask import Flask, send_from_directory
+from werkzeug.contrib.cache import FileSystemCache
+from flask.ext.webcache import easy_setup, modifiers
 
 application = Flask(__name__)
 route = application.route
 application.debug = True
+
+werkzeug_cache = FileSystemCache('/tmp/.sleepycalc')
+easy_setup(application, werkzeug_cache)
 
 
 @route('/')
@@ -20,6 +25,7 @@ def curtime():
 
 
 @route('/badges/<string:repo>/<string:badge>.svg')
+@modifiers.cache_for(seconds=120)
 def send_badges(repo, badge):
     base_path = os.path.join(os.getcwd(), 'static', 'badges', repo)
     return send_from_directory(base_path, '%s.svg' % badge)
